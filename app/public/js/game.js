4b15959630cc4fd6d653dfd82c3d9d9d324e7dc8
byverdu <byverdu@gmail.com>
2014-09-18 17:30:21 +0100

    refactoring and problem image on result page solved

--- app/public/js/game.js
+++ app/public/js/game.js
@@ -22,73 +22,54 @@ jQuery(document).ready(function($) {
 
 	if(window.location.reload && window.location.href.split('9292').pop() === '/game'){
 
-		var game, player, opponent, img_pick, player_img, opponent_img;
+		var game, player, opponent;
 
 		name   = JSON.parse(sessionStorage.getItem('name'));
 
 		player   = new Player(name);
 
-		opponent = new Player('Albert');
+		opponent = new Player('Devil');
 
 		game     = new Game(player,opponent);
 		
 		$('form:last button').click(function(){
 
-			img_pick = $(this).attr('value')
+			var img_pick = $(this).attr('value')
 
 			player.picks(img_pick)
 			opponent.picks(game.randomPick())
-
-			player_img   = $('<img class="img_reult" src="images/'+player.pick+'.png">')
-			opponent_img = $('<img class="img_reult" src="images/'+opponent.pick+'.png">')
 
 			game.winner()
 
 			message = game.displayMessage();
 
-			arr = message.split(' ');
-
-			var index_player   = arr.indexOf(player.pick)
-			var index_opponent = arr.indexOf(opponent.pick)
-
-			arr[index_player]   = player_img
-			arr[index_opponent] = opponent_img
-
-			console.log(player_img)
-
-			sessionStorage.setItem('message', arr);
-			// sessionStorage.setItem('player', JSON.stringify(player.pick));
-			// sessionStorage.setItem('opponent', JSON.stringify(opponent.pick));
-
-			console.log(message)
-
-			console.log('server',player.pick)
-
-			//result = message.split(' ')
-
-			console.log(arr)
+			sessionStorage.setItem('message', JSON.stringify(message));
+			sessionStorage.setItem('player_pick',  player.pick);
+			sessionStorage.setItem('opponent_pick',opponent.pick );
 
 		})
 	}
 
 	if (window.location.reload && window.location.href.split('9292').pop() === '/result') {
 
-			var message, player, render_result;
+			var message, player_pick, opponent_pick, arr, index_player, index_opponent, player_img, opponent_img;
 
-			message = sessionStorage.getItem('message');
-			//player = JSON.parse(sessionStorage.getItem('player'));
+			message = JSON.parse(sessionStorage.getItem('message'));
 
-			render_result = $('#target');
+			player_pick   = sessionStorage.getItem('player_pick')
+			opponent_pick = sessionStorage.getItem('opponent_pick')
+			
+			arr = message.split(' ');
+			
+			index_player   = arr.indexOf(player_pick)
+			index_opponent = arr.indexOf(opponent_pick)
 
-			render_result.text(message)
+			player_img   = $('<img id="pick_'+ player_pick   +'" src="images/'+player_pick+'.png">')
+			opponent_img = $('<img id="pick_'+ opponent_pick +'" src="images/'+opponent_pick+'.png">')
 
-			//render_result.append()
+			arr[index_player]   = player_img
+			arr[index_opponent] = opponent_img
 
-			//console.log('client',player)
-
-			console.log(message)
-
+			$('#target').append(arr)
 	};
-
-	
 });
